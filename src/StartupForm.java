1f2165f8c0e553e3ab9f46e95dec1baf45475659
Phillip Cohen <pc@phillipcohen.net>
2010-04-27 23:42:57 +0000

    Refactored MainForm.

--- src/StartupForm.java
+++ src/StartupForm.java
@@ -9,24 +9,29 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 /**
  * The startup form with buttons to host/connect.
  * @author Phillip Cohen
  */
-public class MainForm extends JFrame implements ActionListener
+public class StartupForm extends JFrame implements ActionListener
 {
+	// The buttons that start a session.
 	private final JButton hostButton, joinButton;
 
-	public MainForm()
+	public StartupForm()
 	{
 		setLayout( new BoxLayout( getContentPane(), BoxLayout.Y_AXIS ) );
 
+		// ======================
 		// Add the title JPanel.
-		final JPanel titlePanel = new JPanel();
+		// ======================
+
+		JPanel titlePanel = new JPanel();
 		{
-			final JLabel titleLabel = new JLabel( "Keck Lab Banner!" );
+			JLabel titleLabel = new JLabel( "Keck Lab Banner!" );
 			titleLabel.setForeground( Color.LIGHT_GRAY );
 			titleLabel.setFont( new Font( "Sans serif", Font.BOLD, 28 ) );
 			titlePanel.add( Box.createRigidArea( new Dimension( 1, 30 ) ) );
@@ -35,22 +40,28 @@ public class MainForm extends JFrame implements ActionListener
 		}
 		add( titlePanel );
 
+		// ==============================
 		// Add the host/connect buttons.
-		hostButton = new JButton( "Start a new session" );
-		joinButton = new JButton( "Join existing session" );
+		// ==============================
 
+		hostButton = new JButton( "Start a new session" );
 		hostButton.addActionListener( this );
+		joinButton = new JButton( "Join existing session" );
 		joinButton.addActionListener( this );
+
 		add( Box.createRigidArea( new Dimension( 1, 25 ) ) );
 		add( hostButton );
 		add( Box.createRigidArea( new Dimension( 1, 5 ) ) );
 		add( joinButton );
 		add( Box.createRigidArea( new Dimension( 1, 35 ) ) );
 
+		// ================
 		// Add the footer.
-		final JPanel footerPanel = new JPanel();
+		// ================
+
+		JPanel footerPanel = new JPanel();
 		{
-			final JLabel footerLabel = new JLabel( "Version 1.0 / created by Phillip Cohen" );
+			JLabel footerLabel = new JLabel( "Version 1.0 / created by Phillip Cohen" );
 			footerLabel.setForeground( Color.gray );
 			footerPanel.add( footerLabel );
 		}
@@ -64,22 +75,27 @@ public class MainForm extends JFrame implements ActionListener
 		setVisible( true );
 	}
 
+	/**
+	 * A button was clicked...
+	 */
 	@Override
 	public void actionPerformed( ActionEvent e )
 	{
 		if ( e.getSource() == hostButton )
 		{
+			// Start the server.
 			setVisible( false );
-			final BannerController controller = new BannerController();
-			final int port = 50903;// Integer.parseInt( JOptionPane.showInputDialog( "Enter the port number" ) );
+			BannerController controller = new BannerController();
+			int port = Integer.parseInt( JOptionPane.showInputDialog( "Enter the port to host on." ) );
 			new BannerFrame( controller, new Server( controller, port ) );
 			dispose();
 		}
 		else if ( e.getSource() == joinButton )
 		{
+			// Start the client.
 			setVisible( false );
-			final BannerController controller = new BannerController();
-			final String input = "10.40.123.23:50903";// JOptionPane.showInputDialog( "Enter the server's address and port." );
+			BannerController controller = new BannerController();
+			String input = JOptionPane.showInputDialog( "Enter the server's address and port." );
 			new BannerFrame( controller, new Client( controller, input.split( ":" )[0], Integer.parseInt( input.split( ":" )[1] ) ) );
 			dispose();
 		}
@@ -92,6 +108,6 @@ public class MainForm extends JFrame implements ActionListener
 	 */
 	public static void main( String[] args )
 	{
-		new MainForm().setVisible( true );
+		new StartupForm().setVisible( true );
 	}
 }
