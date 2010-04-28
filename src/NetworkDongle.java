ed59437407f1306a4769a1729a97ccf6f2a1911e
Phillip Cohen <pc@phillipcohen.net>
2010-04-28 00:03:12 +0000

    Refactored NetworkDongle.

--- src/NetworkDongle.java
+++ src/NetworkDongle.java
@@ -1,14 +1,29 @@
+/**
+ * Either a server or a client. This parent class is used to share code, and let BannerFrame control both seamlessly.
+ */
 public abstract class NetworkDongle
 {
+	/**
+	 * Are we connected?
+	 */
 	protected boolean connected = false;
 
+	/**
+	 * The banner controller used on this computer.
+	 */
 	protected BannerController localController;
 
+	/**
+	 * Base constructor.
+	 */
 	public NetworkDongle( BannerController localController )
 	{
 		this.localController = localController;
 	}
 
+	/**
+	 * Disconnects this server/client.
+	 */
 	public void disconnect()
 	{
 		connected = false;
